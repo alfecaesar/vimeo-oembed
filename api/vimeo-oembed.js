d3c84118745fc95d7f2dbc478ac521736b4ec275
alfecaesar <alfecaesar@gmail.com>
2024-12-19 18:56:55 +0800

    add new headers

--- api/vimeo-oembed.js
+++ api/vimeo-oembed.js
@@ -14,7 +14,10 @@ module.exports = async (req, res) => {
       method: 'GET',
       headers: {
         'Referer': referer,
-        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36'
+        'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.110 Safari/537.36',
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Methods': 'GET',
+        'Access-Control-Allow-Headers': 'Content-Type, Authorization'
       }
     });
 
